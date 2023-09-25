2f297dc71f76e5e3f9a62b0587957173dad71ba8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-09-25 16:27:53 +0300

    Atualizações

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -12,7 +12,7 @@
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <h5>Project Gov+Social - AADESAM</h5>
+    <h5>Gov+Social - AADESAM</h5>
 
     <div class="row mb-1">
       <div class="col">
