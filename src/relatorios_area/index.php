3b36e164861d20f4b5acd2733c5d89939e58c697
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-01-08 11:05:40 +0200

    atualizações

--- src/relatorios_area/index.php
+++ src/relatorios_area/index.php
@@ -1,54 +1,56 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/vns/govSocial/lib/includes.php");
 ?>
-
-<div class="col">
-    <div class="form-floating mb-3">
-        <select required name="municipio" id="municipio" class="form-control" placeholder="Município">
-            <option value="">::Selecione o Município::</option>
-            <?php
-                $q = "select * from municipios order by municipio";
-                $r = mysqli_query($con, $q);
-                while($s = mysqli_fetch_object($r)){
-            ?>
-            <option value="<?=$s->codigo?>" <?=(($d->municipio == $s->codigo)?'selected':false)?>><?=$s->municipio?></option>
-            <?php
-                }
-            ?>
-        </select>
-        <label for="email">Município</label>
+<div class="row">
+    <div class="col">
+        <div class="form-floating mb-3">
+            <select required name="municipio" id="municipio" class="form-control" placeholder="Município">
+                <option value="">::Selecione o Município::</option>
+                <?php
+                    $q = "select * from municipios order by municipio";
+                    $r = mysqli_query($con, $q);
+                    while($s = mysqli_fetch_object($r)){
+                ?>
+                <option value="<?=$s->codigo?>" <?=(($d->municipio == $s->codigo)?'selected':false)?>><?=$s->municipio?></option>
+                <?php
+                    }
+                ?>
+            </select>
+            <label for="email">Município</label>
+        </div>
     </div>
-
-    <div class="form-floating mb-3">
-        <select required name="zona" id="zona" class="form-control" placeholder="Zona">
-            <option value="">::Selecione a Zona::</option>
-            <option value="Urbano" <?=(($d->zona == 'Urbano')?'selected':false)?>>Urbano</option>
-            <option value="Rural" <?=(($d->zona == 'Rural')?'selected':false)?>>Rural</option>
-        </select>
-        <label for="zona">Zona</label>
+    <div class="col">
+        <div class="form-floating mb-3">
+            <select required name="zona" id="zona" class="form-control" placeholder="Zona">
+                <option value="">::Selecione a Zona::</option>
+                <option value="Urbano" <?=(($d->zona == 'Urbano')?'selected':false)?>>Urbano</option>
+                <option value="Rural" <?=(($d->zona == 'Rural')?'selected':false)?>>Rural</option>
+            </select>
+            <label for="zona">Zona</label>
+        </div>
     </div>
-
-    <div class="form-floating mb-3">
-        <select required name="bairro_comunidade" id="bairro_comunidade" class="form-control" placeholder="Bairro">
-            <option value="">::Selecione a Localização::</option>
-            <?php
-                $q = "select * from bairros_comunidades where municipio = '{$d->municipio}' ".(($d->zona)?" and tipo = '{$d->zona}'":false)." order by descricao";
-                $r = mysqli_query($con, $q);
-                while($s = mysqli_fetch_object($r)){
-            ?>
-            <option value="<?=$s->codigo?>" <?=(($d->bairro_comunidade == $s->codigo)?'selected':false)?>><?=$s->descricao?> (<?=$s->tipo?>)</option>
-            <?php
-                }
-            ?>
-        </select>
-        <label for="bairro_comunidade">Bairro/Comunidade</label>
+    <div class="col">
+        <div class="form-floating mb-3">
+            <select required name="bairro_comunidade" id="bairro_comunidade" class="form-control" placeholder="Bairro">
+                <option value="">::Selecione a Localização::</option>
+                <?php
+                    $q = "select * from bairros_comunidades where municipio = '{$d->municipio}' ".(($d->zona)?" and tipo = '{$d->zona}'":false)." order by descricao";
+                    $r = mysqli_query($con, $q);
+                    while($s = mysqli_fetch_object($r)){
+                ?>
+                <option value="<?=$s->codigo?>" <?=(($d->bairro_comunidade == $s->codigo)?'selected':false)?>><?=$s->descricao?> (<?=$s->tipo?>)</option>
+                <?php
+                    }
+                ?>
+            </select>
+            <label for="bairro_comunidade">Bairro/Comunidade</label>
+        </div>
     </div>
 </div>
-
 
 <script>
     $(function(){
         Carregando('none')
-        
+
     })
 </script>
