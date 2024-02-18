0f2d38c00ae6365ec63e1d15e05e22ab3c1fb00b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-18 19:07:54 +0200

    Atualizações

---
+++ mapa.php
@@ -0,0 +1,171 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/vns/govSocial/lib/includes.php");
+    //SELECT *, STR_TO_DATE(FROM_UNIXTIME(dados->>'$.timestamp'/1000),"%Y-%m-%d %H:%i:%s") as data FROM `logLocation` where usuario = 248;
+
+
+    $query = "SELECT * FROM `logLocation` where usuario = 248 and FROM_UNIXTIME(dados->>'$.timestamp'/1000,\"%Y-%m-%d %H:%i:%s\") between '2024-02-17 12:30:00' and '2024-02-17 13:10:00'";
+    $result = mysqli_query($con, $query);
+    while($d = mysqli_fetch_object($result)){
+
+      $dados = json_decode($d->dados);
+
+      echo $dados->coords->latitude." & ".$dados->coords->longitude."<br>";
+
+    }
+
+    exit();
+?>
+
+<html>
+  <head>
+    <title>Directions Service</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+    <style>
+/* Optional: Makes the sample page fill the window. */
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#container {
+  height: 100%;
+  display: flex;
+}
+
+#sidebar {
+  flex-basis: 15rem;
+  flex-grow: 1;
+  padding: 1rem;
+  max-width: 30rem;
+  height: 100%;
+  box-sizing: border-box;
+  overflow: auto;
+}
+
+#map {
+  flex-basis: 0;
+  flex-grow: 4;
+  height: 100%;
+}
+
+#directions-panel {
+  margin-top: 10px;
+}
+    </style>
+    <script>
+function initMap() {
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: { lat: 31.2604054, lng: 29.9872474 },
+  });
+
+  directionsRenderer.setMap(map);
+  document.getElementById("submit").addEventListener("click", () => {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  });
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const waypts = [];
+  const checkboxArray = document.getElementById("waypoints");
+
+  for (let i = 0; i < checkboxArray.length; i++) {
+    if (checkboxArray.options[i].selected) {
+      waypts.push({
+        location: checkboxArray[i].value,
+        stopover: false,
+      });
+    }
+  }
+
+  directionsService
+    .route({
+      origin: document.getElementById("start").value,
+      destination: document.getElementById("end").value,
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+
+      const route = response.routes[0];
+      const summaryPanel = document.getElementById("directions-panel");
+
+      summaryPanel.innerHTML = "";
+
+      // For each route, display summary information.
+      for (let i = 0; i < route.legs.length; i++) {
+        const routeSegment = i + 1;
+
+        summaryPanel.innerHTML +=
+          "<b>Route Segment: " + routeSegment + "</b><br>";
+        summaryPanel.innerHTML += route.legs[i].start_address + " to ";
+        summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
+        summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
+      }
+    })
+    .catch((e) => window.alert("Directions request failed due to " + status));
+}
+
+window.initMap = initMap;
+    </script>
+  </head>
+  <body>
+    <div id="container">
+      <div id="map"></div>
+      <div id="sidebar">
+        <div>
+          <b>Start:</b>
+          <select id="start">
+            <option value="31.2604004,29.988434">Inicial</option>
+            <option value="Boston, MA">Boston, MA</option>
+            <option value="New York, NY">New York, NY</option>
+            <option value="Miami, FL">Miami, FL</option>
+          </select>
+          <br />
+          <b>Waypoints:</b> <br />
+          <i>(Ctrl+Click or Cmd+Click for multiple selection)</i> <br />
+          <select multiple id="waypoints">
+            <option value="31.2602822,29.9887321">Final</option>
+            <option value="toronto, ont">Toronto, ONT</option>
+            <option value="chicago, il">Chicago</option>
+            <option value="winnipeg, mb">Winnipeg</option>
+            <option value="fargo, nd">Fargo</option>
+            <option value="calgary, ab">Calgary</option>
+            <option value="spokane, wa">Spokane</option>
+          </select>
+          <br />
+          <b>End:</b>
+          <select id="end">
+            <option value="31.2604954,29.9888106">Final</option>
+            <option value="Vancouver, BC">Vancouver, BC</option>
+            <option value="Seattle, WA">Seattle, WA</option>
+            <option value="San Francisco, CA">San Francisco, CA</option>
+            <option value="Los Angeles, CA">Los Angeles, CA</option>
+          </select>
+          <br />
+          <input type="submit" id="submit" />
+        </div>
+        <div id="directions-panel"></div>
+      </div>
+    </div>
+
+    <!-- 
+      The `defer` attribute causes the callback to execute after the full HTML
+      document has been parsed. For non-blocking uses, avoiding race conditions,
+      and consistent behavior across browsers, consider loading using Promises.
+      See https://developers.google.com/maps/documentation/javascript/load-maps-js-api
+      for more information.
+      -->
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&callback=initMap&v=weekly"
+      defer
+    ></script>
+  </body>
+</html>
